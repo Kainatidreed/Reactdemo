7412e691203426a7938e4ebf552ddbf6db371238
kainat <im493177@gmail.com>
2024-11-12 03:02:35 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Test from "./Files/Test"
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello my name is kainat idrees</h1>
+      <Test/>
+      
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Files/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+        <h1>Hello i am a test shadow</h1>
+      
+    </div>
+  )
+}
+
+export default Test
